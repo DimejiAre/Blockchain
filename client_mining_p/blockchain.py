69930fab79b1e6193c76a6797e113e46f08e90b8
DimejiAre <olardi2@gmail.com>
2020-03-12 16:58:40 +0100

    feat: add chain data and cors policy to server

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -4,11 +4,105 @@ from time import time
 from uuid import uuid4
 
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 
 class Blockchain(object):
     def __init__(self):
-        self.chain = []
+        self.chain = [
+        {
+            'index': 1,
+            'previous_hash': 1,
+            'proof': 100,
+            'timestamp': 1571852367.484206,
+            'transactions': []
+        },
+        {
+            'index': 2,
+            'previous_hash': "ddf1adddad9af96695fda647492897f058aa702f806d7eaa21dfd46ecab0fcd1",
+            'proof': 24368051,
+            'timestamp': 1571852436.924649,
+            'transactions': [
+                {
+                    'amount': 1,
+                    'recipient': "Brian",
+                    'sender': "0"
+                }
+            ]
+        },
+        {
+            'index': 3,
+            'previous_hash': "2fa2bcc7b423d5d74d621835b098842cf9dd34591bfc0e68800a41cf20b2ec90",
+            'proof': 8132268,
+            'timestamp': 1571852467.247827,
+            'transactions': [
+                {
+                    'amount': 1,
+                    'recipient': "Brian",
+                    'sender': "0"
+                }
+            ]
+        },
+        {
+            'index': 4,
+            'previous_hash': "3f4b18d04371d8ce3129643ccf5ad46bb9943a87adf8c5addded0d3612128f59",
+            'proof': 1301845,
+            'timestamp': 1571852472.199991,
+            'transactions': [
+                {
+                    'amount': 1,
+                    'recipient': "Brian",
+                    'sender': "0"
+                }
+            ]
+        },
+        {
+            'index': 5,
+            'previous_hash': "9177932144818f9c3072d11849251bdd31096621162f0e081016fa59e25010d2",
+            'proof': 13176802,
+            'timestamp': 1571852599.8256152,
+            'transactions': [
+                {
+                    'amount': "3",
+                    'recipient': "Beej",
+                    'sender': "Brian"
+                },
+                {
+                    'amount': 1,
+                    'recipient': "Brian",
+                    'sender': "0"
+                }
+            ]
+        },
+        {
+            'index': 6,
+            'previous_hash': "1e110e46bd7a6a86cd39c3adae667439a40e31b20db3b314bed5b1fa56c746ea",
+            'proof': 41571496,
+            'timestamp': 1571852940.9420102,
+            'transactions': [
+                {
+                    'amount': ".5",
+                    'recipient': "Brady",
+                    'sender': "Beej"
+                },
+                {
+                    'amount': ".5",
+                    'recipient': "Elissa",
+                    'sender': "Beej"
+                },
+                {
+                    'amount': ".5",
+                    'recipient': "Tom",
+                    'sender': "Beej"
+                },
+                {
+                    'amount': 1,
+                    'recipient': "Brian",
+                    'sender': "0"
+                }
+            ]
+        }
+    ]
         self.current_transactions = []
 
         # Create the genesis block
@@ -114,6 +208,7 @@ class Blockchain(object):
 
 # Instantiate our Node
 app = Flask(__name__)
+CORS(app)
 
 # Generate a globally unique address for this node
 node_identifier = str(uuid4()).replace('-', '')
